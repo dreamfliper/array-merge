243437c712f9fe2697cdaa744916b2dd1d7e675d
Henpai Hsu <dreamfliper@gmail.com>
2018-03-15 11:28:58 +0800

    update test

--- test.js
+++ test.js
@@ -1,11 +1,11 @@
 import test from "ava"
 import currify from "./currify"
-import './index'
+import "./index"
 
 test("currify iteration old way", t => {
 	let args = [],
-			old_argument = [2, 3, 5],
-			new_argument = [3, 2, 7, 3, 6]
+		old_argument = [2, 3, 5],
+		new_argument = [3, 2, 7, 3, 6]
 	for (let i = 0, l = new_argument.length; i < l; i++)
 		args[args.length] = new_argument[i]
 	t.deepEqual(args, new_argument)
@@ -20,12 +20,17 @@ test("array-mix", t => {
 	const array1 = [3, 4, 5],
 		array2 = [4, 5, 6]
 	t.deepEqual(array1.mix((x, y) => x + y, array2), [7, 9, 11])
+	t.deepEqual(array1, [3, 4, 5])
+	t.deepEqual(array2, [4, 5, 6])
 })
 
 test("mix-many", t => {
 	const argu1 = (a, c, g) => a + c + g,
-				argu2 = [3, 4, 5],
-				argu3 = [3, 6, 7],
-				argu4 = [2, 3, 7]
+		argu2 = [3, 4, 5],
+		argu3 = [3, 6, 7],
+		argu4 = [2, 3, 7]
 	t.deepEqual(argu2.mix(argu1, argu3, argu4), [8, 13, 19])
+	t.deepEqual(argu2, [3, 4, 5])
+	t.deepEqual(argu3, [3, 6, 7])
+	t.deepEqual(argu4, [2, 3, 7])
 })
