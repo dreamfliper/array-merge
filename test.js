574c16bc893c239b04f1488652c14e28468fe82c
Henpai Hsu <dreamfliper@gmail.com>
2018-03-15 15:26:29 +0800

    add array-diff

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 import test from "ava"
 import currify from "./currify"
 import "./index"
+import "./arrayDiff"
 
 test("currify iteration old way", t => {
 	let args = [],
@@ -24,7 +25,7 @@ test("array-mix", t => {
 	t.deepEqual(array2, [4, 5, 6])
 })
 
-test("mix-many", t => {
+test("array-mix-many", t => {
 	const argu1 = (a, c, g) => a + c + g,
 		argu2 = [3, 4, 5],
 		argu3 = [3, 6, 7],
@@ -34,3 +35,9 @@ test("mix-many", t => {
 	t.deepEqual(argu3, [3, 6, 7])
 	t.deepEqual(argu4, [2, 3, 7])
 })
+
+test('array-diff', t => {
+	const arr1 = [1,2,6,7],
+		arr2 = [6,7,8,9]
+	t.deepEqual(arr1.diff(arr2), [1, 2])
+});
