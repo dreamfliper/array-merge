4a9e3c7ebe971388899ca7dcc7be0991a44f54df
Henpai Hsu <dreamfliper@gmail.com>
2018-03-15 15:33:54 +0800

    syntax update

--- test.js
+++ test.js
@@ -4,9 +4,9 @@ import "./index"
 import "./arrayDiff"
 
 test("currify iteration old way", t => {
-	let args = [],
-		old_argument = [2, 3, 5],
-		new_argument = [3, 2, 7, 3, 6]
+	let args = []
+	const old_argument = [2, 3, 5],
+	const new_argument = [3, 2, 7, 3, 6]
 	for (let i = 0, l = new_argument.length; i < l; i++)
 		args[args.length] = new_argument[i]
 	t.deepEqual(args, new_argument)
@@ -18,18 +18,18 @@ test("currify", t => {
 })
 
 test("array-mix", t => {
-	const array1 = [3, 4, 5],
-		array2 = [4, 5, 6]
+	const array1 = [3, 4, 5]
+	const array2 = [4, 5, 6]
 	t.deepEqual(array1.mix((x, y) => x + y, array2), [7, 9, 11])
 	t.deepEqual(array1, [3, 4, 5])
 	t.deepEqual(array2, [4, 5, 6])
 })
 
 test("array-mix-many", t => {
-	const argu1 = (a, c, g) => a - c + g,
-		argu2 = [3, 4, 5],
-		argu3 = [3, 6, 7],
-		argu4 = [2, 3, 7]
+	const argu1 = (a, c, g) => a - c + g
+	const argu2 = [3, 4, 5]
+	const argu3 = [3, 6, 7]
+	const argu4 = [2, 3, 7]
 	t.deepEqual(argu2.mix(argu1, argu3, argu4), [2, 1, 5])
 	t.deepEqual(argu2, [3, 4, 5])
 	t.deepEqual(argu3, [3, 6, 7])
@@ -37,8 +37,8 @@ test("array-mix-many", t => {
 })
 
 test("array-diff", t => {
-	const arr1 = [1, 2, 6, 7],
-		arr2 = [6, 7, 8, 9]
+	const arr1 = [1, 2, 6, 7]
+	const arr2 = [6, 7, 8, 9]
 	t.deepEqual(arr1.diff(arr2), [1, 2])
 	t.deepEqual(arr1, [1, 2, 6, 7])
 	t.deepEqual(arr2, [6, 7, 8, 9])
